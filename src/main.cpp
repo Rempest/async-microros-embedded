94177df3e698e63ae063e4923fcaa3eee53eae8b
Raempest <thehuman130410@gmail.com>
2026-05-18 17:31:26 +0500

    Initialize micro_ros_async_controller node
    Fix issues with node initialization and executor usage.

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,14 @@
-
+#include <rclcpp/rclcpp.hpp>
+#include  "micro_ros_async_controller/controller.hpp"
+int main(int argc, char ** argv){
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<ControllerNode>();
+  rclcpp::executors::MultiThreadedExecutor executor;
+  excutor.add_node(Node)
+    RCLCPP_INFO(
+    node -> get_logger(), "Starting micro_ros_async_controller node...";
+    );
+  executor.spin();
+  rclcpp::shutdown();
+  return 0;
+}
