1ac8ef56cf2beb4a4b97b3b247e5293d281cdee7
Raempest <thehuman130410@gmail.com>
2026-05-18 16:55:18 +0500

    Initialize ControllerNode with default values
    Add includes and constructor for ControllerNode class

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,1 +1,11 @@
-
+#include "include/micro_ros_async_controller/controller.hpp"
+#include <chrono>
+#include <functional>
+using namespace std::chrono_literals;
+ControllerNode::ControllerNode():Node("micro_ros_async_controller"),
+linear_velocity_(0.0), 
+angular_velocity_(0.0),
+controller_activity(true) {
+  
+}
+  
