72cd6c79b05348827a52cf9daf34f06248e29634
Raempest <thehuman130410@gmail.com>
2026-05-18 17:31:49 +0500

    Fix syntax errors and improve main function structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,24 @@
 #include <rclcpp/rclcpp.hpp>
-#include  "micro_ros_async_controller/controller.hpp"
-int main(int argc, char ** argv){
-  rclcpp::init(argc, argv);
-  auto node = std::make_shared<ControllerNode>();
-  rclcpp::executors::MultiThreadedExecutor executor;
-  excutor.add_node(Node)
+#include "micro_ros_async_controller/controller.hpp"
+
+int main(int argc, char ** argv)
+{
+    rclcpp::init(argc, argv);
+
+    auto node = std::make_shared<ControllerNode>();
+
+    rclcpp::executors::MultiThreadedExecutor executor;
+
+    executor.add_node(node);
+
     RCLCPP_INFO(
-    node -> get_logger(), "Starting micro_ros_async_controller node...";
+        node->get_logger(),
+        "Starting micro_ros_async_controller node..."
     );
-  executor.spin();
-  rclcpp::shutdown();
-  return 0;
+
+    executor.spin();
+
+    rclcpp::shutdown();
+
+    return 0;
 }
